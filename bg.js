9fafcfd1a72329d140b129be35ad31c5f800e916
byungsker <72309817+lbo728@users.noreply.github.com>
2021-02-22 02:42:00 +0900

    final-2

--- bg.js
+++ bg.js
@@ -6,7 +6,7 @@ const IMG_NUMBER = 6;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `/images/${imgNumber + 1}.jpg`;
+    image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
